2e9360a8cd0a6886c653ac41c1bdb7e1ec6368c3
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-15 18:31:03 +0300

    add entity to the database

---
+++ UserManagement.Domain/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace UserManagement.Domain;
+
+public class User : IdentityUser<long>
+{
+    public DateTime LastLoginTime { get; set; }
+    public DateTime RegistrationTime { get; set; }
+    public string Status { get; set; }
+}
