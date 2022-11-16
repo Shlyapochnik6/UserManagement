15142688e6e00ad89eafd40b26157ee5a7fc9226
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-16 17:57:02 +0300

    add services and middleware

--- UserManagement.MVC/Program.cs
+++ UserManagement.MVC/Program.cs
@@ -1,9 +1,52 @@
+using System.Reflection;
+using Microsoft.AspNetCore.Identity;
+using UserManagement.Application;
+using UserManagement.Application.Common.Mappings;
+using UserManagement.Application.Interfaces;
+using UserManagement.Domain;
+using UserManagement.Persistence;
+using UserManagement.Persistence.Contexts;
+using UserManagement.Persistence.Initializers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAutoMapper(config =>
+{
+    config.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
+    config.AddProfile(new AssemblyMappingProfile(typeof(IUserManagementDbContext).Assembly));
+});
+
+builder.Services.AddApplication();
+builder.Services.AddPersistence(builder.Configuration);
+
+builder.Services.AddIdentity<User, IdentityRole<long>>(options =>
+{
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedEmail = false;
+}).AddEntityFrameworkStores<UserManagementDbContext>();
+
 var app = builder.Build();
+
+using (var scope = builder.Services.BuildServiceProvider().CreateScope())
+{
+    var provider = scope.ServiceProvider;
+    try
+    {
+        var context = provider.GetRequiredService<UserManagementDbContext>();
+        DbInitializer.Initialize(context);
+    }
+    catch (Exception e)
+    {
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -18,6 +61,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
