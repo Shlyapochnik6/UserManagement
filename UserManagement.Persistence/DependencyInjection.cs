88e2007aab463a41f41f9aed32401478e9d19c19
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-16 16:05:28 +0300

    add dependency injection and mapping

---
+++ UserManagement.Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserManagement.Application.Interfaces;
+using UserManagement.Persistence.Contexts;
+
+namespace UserManagement.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString = configuration["DbConnection"];
+        services.AddDbContext<UserManagementDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        services.AddScoped<IUserManagementDbContext, UserManagementDbContext>();
+
+        return services;
+    }
+}
