2f1161d849a673eb8e8e8c4c305be680a9a39f05
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-16 16:06:14 +0300

    add dbinitializer

---
+++ UserManagement.Persistence/Initializers/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using UserManagement.Persistence.Contexts;
+
+namespace UserManagement.Persistence.Initializers;
+
+public class DbInitializer
+{
+    public static void Initialize(UserManagementDbContext context)
+    {
+        context.Database.EnsureCreated();
+    }
+}
