e4d5ed4d6f1aa2b813225626cf5ab43c693a2f5c
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-16 20:13:10 +0300

    add registration view model

---
+++ UserManagement.MVC/Models/RegistrationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UserManagement.MVC.Models;
+
+public class RegistrationViewModel
+{
+    [Required] 
+    public string UserName { get; set; }
+    
+    [Required]
+    [EmailAddress(ErrorMessage = "Is not an e-mail")]
+    public string Email { get; set; }
+
+    [Required] 
+    [MinLength(1)]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    [Required]
+    [Compare("Password", ErrorMessage = "Password don't match")]
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm password")]
+    public string PasswordConfirm { get; set; }
+}
