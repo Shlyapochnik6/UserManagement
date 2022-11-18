741fabc772da47dbffee2213733e67c570f7e642
Arseniy Kalenik <dhhxdjsjdh@gmail.com>
2022-11-18 20:14:21 +0300

    add get list users query

---
+++ UserManagement.Application/Queries/User/GetListUsers/GetListUsersQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UserManagement.Application.Interfaces;
+
+namespace UserManagement.Application.Queries.User.GetListUsers;
+
+public class GetListUsersQueryHandler : IRequestHandler<GetListUsersQuery, ListUsersVm>
+{
+    private readonly IUserManagementDbContext _dbContext;
+    private readonly IMapper _mapper;
+    
+    public async Task<ListUsersVm> Handle(GetListUsersQuery request, CancellationToken cancellationToken)
+    {
+        var users = await _dbContext.Users.ToListAsync();
+
+        return new ListUsersVm() { Users = users };
+    }
+}
---
+++ UserManagement.Application/Queries/User/GetListUsers/ListUsersVm.cs
@@ -0,0 +1,6 @@
+﻿namespace UserManagement.Application.Queries.User.GetListUsers;
+
+public class ListUsersVm 
+{
+    public IEnumerable<Domain.User> Users { get; set; }
+}
